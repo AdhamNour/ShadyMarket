06224d9e84a7844eba98e557ceedb3540ee35967
Dj Man <davidmesak@hotmail.com>
2020-12-30 15:58:30 +0200

    Co-authored-by: Adham Nour El-Waffaa Mohammed Hassen Hassen Fouad 1600226 <1600226@eng.asu.edu.eg>

---
+++ shady_market/lib/screens/TransactionsList/TransactionList.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shady_market/providers/CurrentUserProvider.dart';
+import 'package:shady_market/widget/waiting.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class TransactionsPage extends StatelessWidget {
+  Future prepareTransactions(Buildcontext ctx) async {
+    const url = 'http://192.168.1.7:4000/transactions';
+    const headers = {'Content-Type': 'application/json'};
+
+    var body = <String, dynamic>{
+      "token":
+          Provider.of<CurrentUserProvider>(ctx, listen: false).currentUser.id
+    };
+// Sending a POST request with headers
+    http.Response response =
+        await http.post(url, body: jsonEncode(body), headers: headers);
+
+    var result = jsonDecode(response.body);
+    if (result['success']) {
+      //print('auth success');
+      // add id to the person
+      return result;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+            child: FutureBuilder(
+          future: prepareTransactions(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              List<Widget> transactions = snapshot.data
+                  .map((element) => Card(
+                        child: Text("Hiiiiiiii"),
+                        shadowColor: Colors.grey,
+                      ))
+                  .toList();
+              return ListView(
+                children: transactions,
+              );
+            } else if (snapshot.data.length == 0) {
+              return Center(
+                child: Column(
+                  children: [
+                    Image.asset(
+                      "images/empty.png",
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Text("Not Transactions So Far"),
+                    )
+                  ],
+                ),
+              );
+            } else {
+              return Waiting();
+            }
+          },
+        )),
+      ),
+    );
+  }
+}
